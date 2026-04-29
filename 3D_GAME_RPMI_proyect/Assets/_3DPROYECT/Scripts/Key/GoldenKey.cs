95c3e4140605c87857b1875867a1d991886672f2
MauricioSanchezFernandez <mausanfer16@gmail.com>
2026-04-29 14:28:28 +0200

    animacion player

--- 3D_GAME_RPMI_proyect/Assets/_3DPROYECT/Scripts/Key/GoldenKey.cs
+++ 3D_GAME_RPMI_proyect/Assets/_3DPROYECT/Scripts/Key/GoldenKey.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class GoldenKey : MonoBehaviour
 {
+    public GameObject door2;
+
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             GameManager.instance.AddGoldKey();
             Destroy(gameObject);
+            door2.SetActive(true);
+
         }
     }
 }
